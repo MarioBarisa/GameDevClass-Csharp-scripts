6fd6154cdd47b88a69673712a8b3054def161e14
Mario Barisa <mario@barisa.me>
2026-03-25 13:53:52 +0100

    ispravljen Double jump

--- Vj2 - Player 2D movement /Player.cs
+++ Vj2 - Player 2D movement /Player.cs
@@ -3,32 +3,30 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-
     [SerializeField] private float walkSpeed = 4;
     [SerializeField] private float runSpeed = 8;
     [SerializeField] private float jumpForce = 5;
     [SerializeField] InputActionAsset inputActions;
 
     private Rigidbody2D rb;
-    
-    void OnEnable()=> inputActions.FindActionMap("Player").Enable();
-    void OnDisable()=> inputActions.FindActionMap("Player").Disable();
-
-
     private InputAction moveAction;
     private InputAction sprintAction;
     private InputAction jumpAction;
+    private int jumpCount = 0;
+    private const int maxJumps = 2;
+
+    void OnEnable() => inputActions.FindActionMap("Player").Enable();
+    void OnDisable() => inputActions.FindActionMap("Player").Disable();
 
     void Start()
     {
         moveAction = inputActions.FindAction("Move");
         sprintAction = inputActions.FindAction("Sprint");
-        rb = transform.GetComponent<Rigidbody2D>();
         jumpAction = inputActions.FindAction("Jump");
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    [System.Obsolete]
-void Update()
+    void Update()
     {
         float moveX = moveAction.ReadValue<Vector2>().x;
         float speed = sprintAction.IsPressed() ? runSpeed : walkSpeed;
@@ -38,8 +36,16 @@ void Update()
         if (moveX != 0)
             transform.localScale = new Vector3(Mathf.Sign(moveX), 1, 1);
 
-        if (jumpAction.triggered)
+        if (jumpAction.triggered && jumpCount < maxJumps)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jumpCount++;
+        }
     }
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        jumpCount = 0;
+    }
 }
