66bc1db8df9b1189824db3c19df031b316dbcc03
Mario Barisa <mario@barisa.me>
2026-04-10 23:12:11 +0200

    add audio to happy coin

--- Skripte + Sprites Combo/HappyCoin/HappyCoin.cs
+++ Skripte + Sprites Combo/HappyCoin/HappyCoin.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class HappyCoin : MonoBehaviour
 {
+    [SerializeField] private AudioSource audioSource;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
         other.GetComponent<CoinCollector>().AddCoin();
+        AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
         Destroy(gameObject);
     }
 }
